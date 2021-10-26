ea981f415a9909c7eecc1963e65ad6bbd246fde2
xuanRan <xuanran0108@foxmail.com>
2021-10-26 16:26:17 +0800

    1初始化测试：配置文件、相关文件夹以及启动

---
+++ src/main/java/com/xuan/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.xuan.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.xuan.demo.entity")
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
